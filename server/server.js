3609302354515a48e81f1e9bb2e8233d2ce9e07b
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-02-20 08:09:14 +0100

    connecting to db

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 const express = require( 'express' );
+const db = require( './db/conexion' );
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+
+
+// Connect to database
+db.conexion();
 
 app.listen( port, () => {
   console.log( `Server is running in http://localhost:${ port }` );

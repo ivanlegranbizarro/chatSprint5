a092e58e554e9e0efba13b345641dfce98766c54
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-02-20 08:31:42 +0100

    register controller

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,7 +1,25 @@
 const User = require( '../models/userModel' );
 
 const userControllers = {
-  register: async ( req, res ) => { },
+  register: async ( req, res ) => {
+    const { name, email, password, passwordConfirmation } = req.body;
+
+    try {
+      const user = await User.create( { name, email, password, passwordConfirmation } );
+
+      res.status( 201 ).json( {
+        status: 'success',
+        data: {
+          user
+        }
+      } );
+    } catch ( err ) {
+      res.status( 400 ).json( {
+        status: 'fail',
+        message: err
+      } );
+    }
+  },
 
   login: async ( req, res ) => { },
 

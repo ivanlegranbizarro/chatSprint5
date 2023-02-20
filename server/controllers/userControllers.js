9a31a75f927bfc11a53f56ddf7647c9789f44e98
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-02-20 08:48:55 +0100

    implementing JWT

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,4 +1,5 @@
 const User = require( '../models/userModel' );
+const signToken = require( '../helpers/jwt' );
 
 const userControllers = {
   register: async ( req, res ) => {
@@ -7,10 +8,13 @@ const userControllers = {
     try {
       const user = await User.create( { name, email, password, passwordConfirmation } );
 
+      const token = signToken( user._id );
+
       res.status( 201 ).json( {
         success: true,
         data: {
-          user
+          user,
+          token
         }
       } );
     } catch ( err ) {
@@ -21,7 +25,44 @@ const userControllers = {
     }
   },
 
-  login: async ( req, res ) => { },
+  login: async ( req, res ) => {
+    const { email, password } = req.body;
+    try {
+      const user = findOne( { email } );
+
+      if ( !user ) {
+        res.status( 404 ).json( {
+          success: false,
+          message: 'User not found'
+        } );
+      }
+
+      const isCorrect = await user.correctPassword( password, user.password );
+
+      if ( !isCorrect ) {
+        res.status( 400 ).json( {
+          success: false,
+          message: 'Incorrect password'
+        } );
+      }
+
+      const token = signToken( user._id );
+
+      res.status( 200 ).json( {
+        success: true,
+        data: {
+          user,
+          token
+        }
+      } );
+
+    } catch ( err ) {
+      res.status( 400 ).json( {
+        success: false,
+        message: err
+      } );
+    }
+  },
 
   logout: async ( req, res ) => { },
 };

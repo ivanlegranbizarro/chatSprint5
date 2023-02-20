5583564909350418a02d422ca3a147d5f66432cf
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-02-20 08:30:53 +0100

    controllers and routers structure

---
+++ server/controllers/userControllers.js
@@ -0,0 +1,11 @@
+const User = require( '../models/userModel' );
+
+const userControllers = {
+  register: async ( req, res ) => { },
+
+  login: async ( req, res ) => { },
+
+  logout: async ( req, res ) => { },
+};
+
+module.exports = userControllers;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,2 +1,12 @@
-const User = require( '../models/userModel' );
 const router = require( 'express' ).Router();
+const userControllers = require( '../controllers/userControllers' );
+
+
+router.post( '/register', userControllers.register );
+
+router.post( '/login', userControllers.login );
+
+router.get( '/logout', userControllers.logout );
+
+
+module.exports = router;

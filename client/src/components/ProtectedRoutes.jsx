590d0b4ad5fc46a961360739e26f1597e067c744
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-02-20 19:47:23 +0100

    layout chat

--- client/src/components/ProtectedRoutes.jsx
+++ client/src/components/ProtectedRoutes.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 import { GetCurrentUser } from '../apicalls/users';
 import { toast, ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
+import { useDispatch, useSelector } from 'react-redux';
+import { showLoader, hideLoader } from '../redux/loaderSlice';
+import { setUser } from '../redux/userSlice';
 
 function ProtectedRoutes ( { children } ) {
-  const [ user, setUser ] = React.useState( null );
+  const { user } = useSelector( state => state.userReducer );
+  const dispatch = useDispatch();
   const [ isAuthenticated, setIsAuthenticated ] = React.useState( false );
 
   const getCurrentUser = async () => {
     try {
+      dispatch( showLoader() );
       const response = await GetCurrentUser();
+      dispatch( hideLoader() );
       if ( response.success ) {
-        setUser( response.data.user );
+        dispatch( setUser( response.data.user ) );
         return true;
       } else {
         toast.error( response.message, {
@@ -21,6 +27,7 @@ function ProtectedRoutes ( { children } ) {
         return false;
       }
     } catch ( error ) {
+      dispatch( hideLoader() );
       toast.error( error.message, {
         position: 'top-center',
       } );
@@ -41,12 +48,22 @@ function ProtectedRoutes ( { children } ) {
   }, [] );
 
   return (
-    <div>
-      {isAuthenticated && children}
-      {user?.name}
-      <br />
-      {user?.email}
-      <ToastContainer />
+    <div className='h-screen w-screen bg-gray-100 p-2'>
+      <div className='flex justify-between p-5'>
+        <div className='flex items-center gap-1'>
+          <i className='ri-message-3-line text-2xl'></i>
+          <h1 className='text-green-500 text-2xl uppercase font-semibold'>Chat</h1>
+        </div>
+        <div className='flex gap-1 text-xl'>
+          <i className='ri-shield-user-line'></i>
+          <h1 className='underline'>{user?.name}</h1>
+        </div>
+      </div>
+      <div className='p-5'>
+        {/* content */}
+        {isAuthenticated && children}
+        <ToastContainer />
+      </div>
     </div>
   );
 }
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import loaderReducer from "./loaderSlice";
+import userReducer from "./userSlice";
 
 const store = configureStore( {
   reducer: {
-    loader: loaderReducer,
+    loaderReducer,
+    userReducer,
   },
 } );
 
